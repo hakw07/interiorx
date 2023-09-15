4b871089b1b52a0ce019bd72d39589c1b5d4a9b6
Md Riazul Islam <riazul5207@gmail.com>
2023-09-15 23:20:05 +0600

    footer section completed💥

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Topbar from '../components/Topbar';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <div className="container">
                 <h1>Hello!</h1>
             </div>
+            <Footer/>
         </div>
     );
 }
