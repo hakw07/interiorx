8f1fb5c946486341c428c914c8e9b57b85224a8d
Md Riazul Islam <riazul5207@gmail.com>
2023-09-17 00:06:39 +0600

    hero slider text animation added💨

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Carousel from 'react-bootstrap/Carousel';
+import 'animate.css';
 
 const HeroSlider = () => {
     const [index, setIndex] = useState(0);
@@ -13,19 +14,19 @@ const HeroSlider = () => {
         <Carousel activeIndex={index} onSelect={handleSelect}>
             <Carousel.Item className="carouselItem carouselItem1" interval={1000}>
                 <Carousel.Caption className="carouselCaption">
-                    <h1>We create beautiful home interior design</h1>
-                    <Link to="/interior" className="mt-4 d-inline-block text-decoration-none text-white bg-primary link">Learn More</Link>
+                    <h1 className="animate__animated animate__fadeInDown">We create beautiful home interior design</h1>
+                    <Link to="/interior" className="animate__animated animate__fadeInDown mt-4 d-inline-block text-decoration-none text-white bg-primary link">Learn More</Link>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item className="carouselItem carouselItem2" interval={500}>
                 <Carousel.Caption className="carouselCaption">
-                    <h1>Luxury Interior design for your dream living</h1>
-                    <Link to="/interior" className="mt-4 d-inline-block text-decoration-none text-white bg-primary link">Learn More</Link>              </Carousel.Caption>
+                    <h1 className="animate__animated animate__fadeInDown">Luxury Interior design for your dream living</h1>
+                    <Link to="/interior" className="animate__animated animate__fadeInDown mt-4 d-inline-block text-decoration-none text-white bg-primary link">Learn More</Link>              </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item className="carouselItem carouselItem3">
                 <Carousel.Caption className="carouselCaption">
-                    <h1>Giving your home a modern and unique style</h1>
-                    <Link to="/interior" className="mt-4 d-inline-block text-decoration-none text-white bg-primary link">Learn More</Link>                </Carousel.Caption>
+                    <h1 className="animate__animated animate__fadeInDown">Giving your home a modern and unique style</h1>
+                    <Link to="/interior" className="animate__animated animate__fadeInDown mt-4 d-inline-block text-decoration-none text-white bg-primary link">Learn More</Link>                </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
     );
