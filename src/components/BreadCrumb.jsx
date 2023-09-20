5879f00e0a1effb77b7223e58b58eee86ce88008
Md Riazul Islam <riazul5207@gmail.com>
2023-09-21 00:37:49 +0600

    cleaned up🧹

--- src/components/BreadCrumb.jsx
+++ src/components/BreadCrumb.jsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 
 const BreadCrumb = ({page}) => {
     return (
-        <div class="breadcrumb">
-            <div class="container-lg d-flex justify-content-between align-items-center">
-                <h2 class="my-0 text-uppercase">About us</h2>
+        <div className="breadcrumb">
+            <div className="container-lg d-flex justify-content-between align-items-center">
+                <h2 className="my-0 text-uppercase">About us</h2>
 
-                <ul class="my-0 d-flex align-items-center">
-                    <li><Link to="/" className="link">Home /</Link></li>
+                <ul className="my-0 d-flex align-items-center">
+                    <li><Link to="/" className="link">Home <span>/</span></Link></li>
                     {page === 'about' && <li aria-current="page"><Link to="/about" className="link active">About</Link></li>}
                     {page === 'interior' && (
                         <>
-                            <li><Link to="/interior" className="link">Our Services /</Link></li>
+                            <li><Link to="/interior" className="link">Our Services <span>/</span></Link></li>
                             <li aria-current="page"><Link to="/interior" className="link active">Interior</Link></li>
                         </>
                     )}
                     {page === 'construction' && (
                         <>
-                            <li><Link to="/construction" className="link">Our Services /</Link></li>
+                            <li><Link to="/construction" className="link">Our Services <span>/</span></Link></li>
                             <li aria-current="page"><Link to="/construction" className="link active">Construction</Link></li>
                         </>
                     )}
