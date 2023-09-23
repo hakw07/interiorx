27ab6965bfbe9836ee1d51dd1680388728d215f3
Md Riazul Islam <riazul5207@gmail.com>
2023-09-24 00:44:58 +0600

    project gallary completed💥

--- src/pages/ProjectGallary.jsx
+++ src/pages/ProjectGallary.jsx
@@ -17,8 +17,20 @@ import InteriorImg09 from '../assets/images/interior/09.jpg';
 import InteriorImg10 from '../assets/images/interior/10.jpg';
 import InteriorImg11 from '../assets/images/interior/11.jpg';
 import InteriorImg12 from '../assets/images/interior/12.jpg';
+import ConstructionImg01 from '../assets/images/construction/01.jpg';
+import ConstructionImg02 from '../assets/images/construction/02.jpg';
+import ConstructionImg03 from '../assets/images/construction/03.jpg';
+import ConstructionImg04 from '../assets/images/construction/04.jpg';
+import ConstructionImg05 from '../assets/images/construction/05.jpg';
+import ConstructionImg06 from '../assets/images/construction/06.jpg';
+import ConstructionImg07 from '../assets/images/construction/07.jpg';
+import ConstructionImg08 from '../assets/images/construction/08.jpg';
+import ConstructionImg09 from '../assets/images/construction/09.jpg';
+import ConstructionImg10 from '../assets/images/construction/10.jpg';
+import ConstructionImg11 from '../assets/images/construction/11.jpg';
+import ConstructionImg12 from '../assets/images/construction/12.jpg';
 
-const ProjectGallary = () => {
+const ProjectGallary = ({ type="construction" }) => {
     return (
         <AppLayout>
             <BreadCrumb page="project-interior" />
@@ -27,62 +39,74 @@ const ProjectGallary = () => {
                 <div className="gallaryContents">
                     <div className="filterItems projectItems" data-aos="fade-up">                    
                         <div className="card">
-                            <img src={InteriorImg03} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg03} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg03} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
 
                         <div className="card">
-                            <img src={InteriorImg02} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg02} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg02} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
 
                         <div className="card">
-                            <img src={InteriorImg01} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg01} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg01} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
                         
                         <div className="card">
-                            <img src={InteriorImg07} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg07} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg07} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
 
                         <div className="card">
-                            <img src={InteriorImg04} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg04} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg04} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
 
                         <div className="card">
-                            <img src={InteriorImg06} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg06} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg06} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
 
                         <div className="card">
-                            <img src={InteriorImg05} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg05} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg05} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
 
                         <div className="card">
-                            <img src={InteriorImg11} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg11} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg11} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
 
                         <div className="card">
-                            <img src={InteriorImg12} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg12} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg12} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
 
                         <div className="card">
-                            <img src={InteriorImg08} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg08} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg08} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
 
                         <div className="card">
-                            <img src={InteriorImg09} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg09} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg09} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
 
                         <div className="card">
-                            <img src={InteriorImg10} alt="projectImg"/>
+                            {type === "interior" && <img src={InteriorImg10} alt="projectImg"/>}
+                            {type === "construction" && <img src={ConstructionImg10} alt="projectImg"/>}
                             <p className="hoverText"><FaLink/></p>
                         </div>
                     </div>
